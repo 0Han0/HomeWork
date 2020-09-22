f139e678932e16d5d1bfe25520d83f8b24f501ca
Ildar Zalyaliev <vip.attano@inbox.com>
2020-09-23 01:04:11 +0300

    Calc

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using dNET;
+using System.Diagnostics.CodeAnalysis;
+
+namespace dNET
+{
+    [ExcludeFromCodeCoverage]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Calculator.GetNubmer();
+            var b = Calculator.GetNubmer();
+            Console.WriteLine(Calculator.Calculate(a,"+",b));
+        }
+    }
+}
